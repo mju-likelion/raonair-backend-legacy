45d2498249c0552210f9d5d6d1608c5636ed3996
Bhanjo <hanjo1515@naver.com>
2021-06-24 00:06:24 +0900

    홈페이지, 검색결과 디자인

--- actProjects/actProject/settings.py
+++ actProjects/actProject/settings.py
@@ -1,6 +1,7 @@
 
 from pathlib import Path
 from . import my_settings
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -101,8 +102,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
