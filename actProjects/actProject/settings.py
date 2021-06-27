a2fee6361ac852f82c7a62b89a41d44044e918fb
junseokieo <ccobaltbluee@gmail.com>
2021-06-27 20:31:56 +0900

    이 달의 공연 상세내용 내 버튼 추가, 캐시 관련 설정 추가

--- actProjects/actProject/settings.py
+++ actProjects/actProject/settings.py
@@ -111,3 +111,9 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+    }
+}
