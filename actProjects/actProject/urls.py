2a1903eb03d8bb361e54ac5aed8d0cad55f86cf4
alswjd <ypd06021@naver.com>
2021-07-01 15:49:08 +0900

    코드리뷰 수정

--- actProjects/actProject/urls.py
+++ actProjects/actProject/urls.py
@@ -7,9 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', App.views.home, name="home"),
     path('searchPage/', App.views.searchPage, name="searchPage"),
-    path('detail/', admin.site.urls),
     path('detailpage/', App.views.detailpage, name="detailpage"),
-    path('troupelist/', admin.site.urls),
     path('listpage/', App.views.listpage, name="listpage"),
 ]
 
