a23e8f6bd36bf9ffa0b5d94c15e2ccc5f5ef1650
rkdalswjd <ypd06021@naver.com>
2021-08-01 17:36:53 +0900

    리팩토링: models.py 코드 수정

--- actProjects/App/models.py
+++ actProjects/App/models.py
@@ -8,6 +8,75 @@
 from django.db import models
 
 
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.IntegerField()
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.IntegerField()
+    is_active = models.IntegerField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
+
 class Comment(models.Model):
     comment = models.CharField(max_length=200)
     play = models.ForeignKey('Play', models.DO_NOTHING, db_column='play')
@@ -16,6 +85,51 @@ class Comment(models.Model):
     class Meta:
         managed = False
         db_table = 'comment'
+
+
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.PositiveSmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
 
 
 class Like(models.Model):
@@ -29,6 +143,7 @@ class Like(models.Model):
 
 class Person(models.Model):
     name = models.CharField(max_length=255)
+    photo = models.CharField(unique=True, max_length=255, blank=True, null=True)
 
     class Meta:
         managed = False
@@ -55,32 +170,24 @@ class Play(models.Model):
         db_table = 'play'
 
 
-class Rating(models.Model):
+class Staff(models.Model):
+    person = models.ForeignKey(Person, models.DO_NOTHING, db_column='person', blank=True, null=True)
+    play = models.ForeignKey(Play, models.DO_NOTHING, db_column='play', blank=True, null=True)
+    role = models.CharField(max_length=8)
+
+    class Meta:
+        managed = False
+        db_table = 'staff'
+
+
+class Star(models.Model):
     rating = models.DecimalField(max_digits=2, decimal_places=1)
     play = models.ForeignKey(Play, models.DO_NOTHING, db_column='play')
     user = models.ForeignKey('User', models.DO_NOTHING, db_column='user')
 
     class Meta:
         managed = False
-        db_table = 'rating'
-
-
-class Role(models.Model):
-    person = models.ForeignKey(Person, models.DO_NOTHING, blank=True, null=True)
-    type = models.CharField(max_length=4)
-
-    class Meta:
-        managed = False
-        db_table = 'role'
-
-
-class Staff(models.Model):
-    person = models.ForeignKey(Person, models.DO_NOTHING, db_column='person', blank=True, null=True)
-    play = models.ForeignKey(Play, models.DO_NOTHING, db_column='play', blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'staff'
+        db_table = 'star'
 
 
 class Team(models.Model):
