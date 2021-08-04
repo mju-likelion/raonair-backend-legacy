4c3ed6a96f0cc09c11e026a914a9febe28023696
Bhanjo <hanjo1515@naver.com>
2021-08-04 22:31:30 +0900

    추가: 평균 평점 기능 추가

--- actProjects/App/views.py
+++ actProjects/App/views.py
@@ -10,16 +10,14 @@ def home(request) :
     return JsonResponse({"request":'home.html'})
 
 def search(request) :
-    # 검색어 쿼리, 두 번째 요소에는 기본값을 넣어야한다 (임시로 POST를 넣음)
-    # 검색어를 어떻게 받아와야할지 감이 안잡힘
-    keyword = request.POST.get('q', "")
-    plays = models.Play.objects.filter(title__icontains=keyword) # play 모든 결과 불러옴
+    keyword = request.GET.get('q', "")
+    plays = models.Play.objects.filter(title__icontains=keyword) # 검색어에 포함되는 play를 받아옴
 
     # 검색결과가 0개일 때 return
     if(len(plays) == 0):
         return JsonResponse({
             "error": {
-                # "query": str,
+                "query": keyword,
                 "error_meessage": '검색 결과가 없습니다',
             }
         })
@@ -27,23 +25,30 @@ def search(request) :
     ongoing_list = []
     tobe_list = []
     closed_list = []
-
     tody = datetime.strftime(datetime.now(), '%Y-%m-%d')
 
     for i in plays:
-        # 날짜 비교
         start_date = datetime.strftime(i.start_date, '%Y-%m-%d')
         end_date = datetime.strftime(i.end_date, '%Y-%m-%d') if (i.end_date) else None
+        stars = models.Star.objects.filter(play = i.id)
+        
+        #평균 별점 구하기
+        star_sum = 0
+        for j in stars:
+            star_sum += j.star
+        star_avg = star_sum / len(stars) / 2
 
         new_play = ({
             "title": i.title,
             "poster": i.poster,
             "start_date": start_date,
             "end_date": end_date,
+            "star_avg": star_avg,
 
             "keyword": keyword, # 쿼리 확인용
         })
 
+        # 날짜 비교
         if(tody >= start_date and (end_date == None or tody <= end_date)):
             ongoing_list.append(new_play)
         elif(tody < start_date):
@@ -53,9 +58,10 @@ def search(request) :
         else:
             print("날짜설정에러")
 
+    # 출력할 때 앞에서부터? 뒤에서부터?
     return JsonResponse({
         "data": {
-            # "query": str,
+            "query": keyword,
             "searched_results": {
                 "ongoing_plays": ongoing_list[0:4],
                 "tobe_plays": tobe_list[0:4],
