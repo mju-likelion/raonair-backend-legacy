188471e219716bf96ebdae1352de415e996590e8
junseokieo <ccobaltbluee@gmail.com>
2021-08-04 18:29:18 +0900

    추가: search-detail-api 브랜치 생성

--- actProjects/App/models.py
+++ actProjects/App/models.py
@@ -7,6 +7,76 @@
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
+from django.db import models
+
+
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.IntegerField()
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.IntegerField()
+    is_active = models.IntegerField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+class AuthUserGroups(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
 
 class Comment(models.Model):
     comment = models.CharField(max_length=200)
@@ -16,6 +86,50 @@ class Comment(models.Model):
     class Meta:
         managed = False
         db_table = 'comment'
+
+
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.PositiveSmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
 
 
 class Like(models.Model):
@@ -29,11 +143,11 @@ class Like(models.Model):
 
 class Person(models.Model):
     name = models.CharField(max_length=255)
+    photo = models.CharField(unique=True, max_length=255, blank=True, null=True)
 
     class Meta:
         managed = False
         db_table = 'person'
-
 
 class Play(models.Model):
     title = models.CharField(max_length=255)
@@ -42,7 +156,6 @@ class Play(models.Model):
     start_date = models.DateField(db_column='start_DATE')  # Field name made lowercase.
     end_date = models.DateField(db_column='end_DATE', blank=True, null=True)  # Field name made lowercase.
     running_time = models.IntegerField()
-    price = models.IntegerField()
     troupe = models.ForeignKey('Troupe', models.DO_NOTHING, db_column='troupe')
     theater = models.ForeignKey('Theater', models.DO_NOTHING, db_column='theater')
     yes24_external_link = models.CharField(unique=True, max_length=255, blank=True, null=True)
@@ -55,32 +168,24 @@ class Play(models.Model):
         db_table = 'play'
 
 
-class Rating(models.Model):
+class Staff(models.Model):
+    person = models.ForeignKey(Person, models.DO_NOTHING, db_column='person', blank=True, null=True)
+    play = models.ForeignKey(Play, models.DO_NOTHING, db_column='play', blank=True, null=True)
+    role = models.CharField(max_length=8)
+
+    class Meta:
+        managed = False
+        db_table = 'staff'
+
+
+class Star(models.Model):
     rating = models.DecimalField(max_digits=2, decimal_places=1)
     play = models.ForeignKey(Play, models.DO_NOTHING, db_column='play')
     user = models.ForeignKey('User', models.DO_NOTHING, db_column='user')
 
     class Meta:
         managed = False
-        db_table = 'rating'
-
-
-class Role(models.Model):
-    person = models.ForeignKey(Person, models.DO_NOTHING, blank=True, null=True)
-    type = models.CharField(max_length=4)
-
-    class Meta:
-        managed = False
-        db_table = 'role'
-
-
-class Staff(models.Model):
-    person = models.ForeignKey(Person, models.DO_NOTHING, db_column='person', blank=True, null=True)
-    play = models.ForeignKey(Play, models.DO_NOTHING, db_column='play', blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'staff'
+        db_table = 'star'
 
 
 class Team(models.Model):
@@ -90,7 +195,6 @@ class Team(models.Model):
     class Meta:
         managed = False
         db_table = 'team'
-
 
 class Theater(models.Model):
     name = models.CharField(unique=True, max_length=255)
--- actProjects/App/views.py
+++ actProjects/App/views.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse
 from django.shortcuts import render
-
+from . import models
 import base64
 import os
 
@@ -8,27 +8,59 @@ import os
 def home(request) :
     return JsonResponse({"request":'home.html'})
 
-def searchPage(request) :
-    return JsonResponse({"request":'searchPage.html'})
-
+# 검색 결과 페이지, 더보기 클릭 (GET /api/search/:type)
 def detailpage(request) :
+    plays = models.Play.objects.get()
+    #links
+    #start
+    #limit
+    start_date = models.Play.objects.get()
+
+    # 검색 결과가 0개 일 때
+    if(len(plays) == 0):
+        return JsonResponse({
+            "error": {
+                #"query": string, #입력 받은 검색어
+                #"type": enum()
+            }
+        })
+
+    # 더보기를 눌러서 더 많은 공연을 조회했을 때
     return JsonResponse({
-        #"request":"detailpage.html"
-
-    })
-
-def listpage(request) :
-    return  JsonResponse({"request": "listpage.html"})
-
-def search_play_with_options(request) :
-    return JsonResponse({
+        "links": {
+            # next 가 null이 아니라면
+            #"next": string ,
+        },
         "data": {
-            "page": "searchPlayWithOptions"
-        }
+            #"query": string,
+            #"type": enum("ongoing", "tobe", "closed"),
+            #"search_results": play_simple[10],
+        },
     })
 
-def theaterDetail(request) :
-    return JsonResponse({'request': 'theater_detail.html'})
+def troupe(request) :
+    return JsonResponse({"request": "listpage.html"})
 
-def is_poster(image_name) :
-    return True if (('home' in image_name) or ('theme' in image_name) or ('month' in image_name)) else False
+def play(request) :
+    return JsonResponse({"request":"play.html"})
+
+def signin(request) :
+    return  JsonResponse({"request": "signin.html"})
+
+def signup(request) :
+    return  JsonResponse({"request": "signup.html"})
+
+def password(request) :
+    return  JsonResponse({"request": "find-password.html"})
+
+def playlike(request) :
+    return  JsonResponse({"request": "playlike.html"})
+
+def troupelike(request) :
+    return  JsonResponse({"request": "troupelike.html"})
+
+def star(request) :
+    return  JsonResponse({"request": "star.html"})
+
+def comment(request) :
+    return  JsonResponse({"request": "comment.html"})
