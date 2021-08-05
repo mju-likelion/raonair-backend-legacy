df05d0c14be23137ab48ca399c02610677565ba1
Bhanjo <hanjo1515@naver.com>
2021-08-06 00:39:08 +0900

    추가: 검색 결과(극단) 브랜치 생성

--- actProjects/App/models.py
+++ actProjects/App/models.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # This is an auto-generated Django model module.
 # You'll have to do the following manually to clean this up:
 #   * Rearrange models' order
@@ -8,19 +9,138 @@
 from django.db import models
 
 
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.IntegerField()
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.IntegerField()
+    is_active = models.IntegerField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
+
 class Comment(models.Model):
     comment = models.CharField(max_length=200)
     play = models.ForeignKey('Play', models.DO_NOTHING, db_column='play')
     user = models.ForeignKey('User', models.DO_NOTHING, db_column='user')
+    updated_at = models.DateTimeField()
+    created_at = models.DateTimeField()
 
     class Meta:
         managed = False
         db_table = 'comment'
 
 
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.PositiveSmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey(
+        'DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
+
+
 class Like(models.Model):
     play = models.ForeignKey('Play', models.DO_NOTHING, db_column='play')
     user = models.ForeignKey('User', models.DO_NOTHING, db_column='user')
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
 
     class Meta:
         managed = False
@@ -29,6 +149,10 @@ class Like(models.Model):
 
 class Person(models.Model):
     name = models.CharField(max_length=255)
+    photo = models.CharField(
+        unique=True, max_length=255, blank=True, null=True)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
 
     class Meta:
         managed = False
@@ -37,55 +161,66 @@ class Person(models.Model):
 
 class Play(models.Model):
     title = models.CharField(max_length=255)
-    location = models.CharField(max_length=14)
     poster = models.CharField(unique=True, max_length=255)
-    start_date = models.DateField(db_column='start_DATE')  # Field name made lowercase.
-    end_date = models.DateField(db_column='end_DATE', blank=True, null=True)  # Field name made lowercase.
+    # Field name made lowercase.
+    start_date = models.DateField(db_column='start_DATE')
+    # Field name made lowercase.
+    end_date = models.DateField(db_column='end_DATE', blank=True, null=True)
     running_time = models.IntegerField()
-    price = models.IntegerField()
     troupe = models.ForeignKey('Troupe', models.DO_NOTHING, db_column='troupe')
-    theater = models.ForeignKey('Theater', models.DO_NOTHING, db_column='theater')
-    yes24_external_link = models.CharField(unique=True, max_length=255, blank=True, null=True)
-    interpark_external_link = models.CharField(unique=True, max_length=255, blank=True, null=True)
-    playdb_external_link = models.CharField(db_column='playDB_external_link', unique=True, max_length=255, blank=True, null=True)  # Field name made lowercase.
-    culturegov_external_link = models.CharField(db_column='cultureGov_external_link', unique=True, max_length=255, blank=True, null=True)  # Field name made lowercase.
+    theater = models.ForeignKey(
+        'Theater', models.DO_NOTHING, db_column='theater')
+    yes24_external_link = models.CharField(
+        unique=True, max_length=255, blank=True, null=True)
+    interpark_external_link = models.CharField(
+        unique=True, max_length=255, blank=True, null=True)
+    # Field name made lowercase.
+    playdb_external_link = models.CharField(
+        db_column='playDB_external_link', unique=True, max_length=255, blank=True, null=True)
+    # Field name made lowercase.
+    culturegov_external_link = models.CharField(
+        db_column='cultureGov_external_link', unique=True, max_length=255, blank=True, null=True)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
 
     class Meta:
         managed = False
         db_table = 'play'
 
 
-class Rating(models.Model):
-    rating = models.DecimalField(max_digits=2, decimal_places=1)
-    play = models.ForeignKey(Play, models.DO_NOTHING, db_column='play')
-    user = models.ForeignKey('User', models.DO_NOTHING, db_column='user')
-
-    class Meta:
-        managed = False
-        db_table = 'rating'
-
-
-class Role(models.Model):
-    person = models.ForeignKey(Person, models.DO_NOTHING, blank=True, null=True)
-    type = models.CharField(max_length=4)
-
-    class Meta:
-        managed = False
-        db_table = 'role'
-
-
 class Staff(models.Model):
-    person = models.ForeignKey(Person, models.DO_NOTHING, db_column='person', blank=True, null=True)
-    play = models.ForeignKey(Play, models.DO_NOTHING, db_column='play', blank=True, null=True)
+    person = models.ForeignKey(
+        Person, models.DO_NOTHING, db_column='person', blank=True, null=True)
+    play = models.ForeignKey(Play, models.DO_NOTHING,
+                             db_column='play', blank=True, null=True)
+    role = models.CharField(max_length=8)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
 
     class Meta:
         managed = False
         db_table = 'staff'
 
 
+class Star(models.Model):
+    play = models.ForeignKey(Play, models.DO_NOTHING, db_column='play')
+    user = models.ForeignKey('User', models.DO_NOTHING, db_column='user')
+    star = models.DecimalField(max_digits=2, decimal_places=0)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'star'
+
+
 class Team(models.Model):
-    person = models.ForeignKey(Person, models.DO_NOTHING, db_column='person', blank=True, null=True)
-    troupe = models.ForeignKey('Troupe', models.DO_NOTHING, db_column='troupe', blank=True, null=True)
+    person = models.ForeignKey(
+        Person, models.DO_NOTHING, db_column='person', blank=True, null=True)
+    troupe = models.ForeignKey(
+        'Troupe', models.DO_NOTHING, db_column='troupe', blank=True, null=True)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
 
     class Meta:
         managed = False
@@ -94,9 +229,14 @@ class Team(models.Model):
 
 class Theater(models.Model):
     name = models.CharField(unique=True, max_length=255)
+    location = models.CharField(max_length=14, blank=True, null=True)
     address = models.CharField(max_length=255)
-    longitude = models.DecimalField(max_digits=11, decimal_places=8, blank=True, null=True)
-    latitude = models.DecimalField(max_digits=10, decimal_places=8, blank=True, null=True)
+    longitude = models.DecimalField(
+        max_digits=11, decimal_places=8, blank=True, null=True)
+    latitude = models.DecimalField(
+        max_digits=10, decimal_places=8, blank=True, null=True)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
 
     class Meta:
         managed = False
@@ -107,6 +247,8 @@ class Troupe(models.Model):
     name = models.CharField(unique=True, max_length=255)
     type = models.CharField(max_length=7)
     logo = models.CharField(unique=True, max_length=255, blank=True, null=True)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
 
     class Meta:
         managed = False
@@ -114,8 +256,12 @@ class Troupe(models.Model):
 
 
 class TroupeLike(models.Model):
-    troupe = models.ForeignKey(Troupe, models.DO_NOTHING, db_column='troupe', blank=True, null=True)
-    user = models.ForeignKey('User', models.DO_NOTHING, db_column='user', blank=True, null=True)
+    troupe = models.ForeignKey(
+        Troupe, models.DO_NOTHING, db_column='troupe', blank=True, null=True)
+    user = models.ForeignKey('User', models.DO_NOTHING,
+                             db_column='user', blank=True, null=True)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
 
     class Meta:
         managed = False
@@ -128,6 +274,8 @@ class User(models.Model):
     name = models.CharField(max_length=10)
     nickname = models.CharField(unique=True, max_length=10)
     email_confirmed = models.IntegerField()
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
 
     class Meta:
         managed = False
--- actProjects/actProject/urls.py
+++ actProjects/actProject/urls.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 from django.urls import path
 
@@ -5,11 +6,20 @@ import App.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', App.views.home, name="home"),
-    path('searchPage/', App.views.searchPage, name="searchPage"),
-    path('detailpage/', App.views.detailpage, name="detailpage"),
-    path('listpage/', App.views.listpage, name="listpage"),
-    path('search-with-options', App.views.search_play_with_options),
-    path('theaterDetail', App.views.theaterDetail, name="theater_detail"),
+    path('api/home', App.views.home, name='home'),  # 홈페이지
+    path('api/search/play', App.views.search_play,
+         name='search_play'),  # 검색결과페이지
+    path('api/search/troupe', App.views.search_troupe,
+         name='search_troupe'),  # 검색결과페이지
+    path('api/search/:category', App.views.search_detail,
+         name='search_detail'),  # 검색결과페이지, 더보기 클릭
+    path('api/troupe/:id', App.views.troupe, name='troupe'),  # 극단개별페이지
+    path('api/play/<int:id>', App.views.play, name='play'),  # 연극개별페이지
+    path('api/auth/signin', App.views.signin),  # 로그인
+    path('api/auth/signup', App.views.signup, name='signup'),  # 회원가입
+    path('api/auth/find-password', App.views.password, name='password'),  # 비밀번호찾기
+    path('api/plays/:id/star', App.views.star, name='star'),  # 별점
+    path('api/plays/:id/liked', App.views.playlike, name='playlike'),  # 연극찜하기
+    path('api/troupes/:id/like', App.views.troupelike, name='troupelike'),  # 극단찜하기
+    path('api/plays/:id/comment', App.views.comment, name='comment'),  # 커멘트달기
 ]
-
