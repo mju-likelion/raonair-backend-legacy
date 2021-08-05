79671572d149b8a1a04344019e348daa8b0abb40
junseokieo <ccobaltbluee@gmail.com>
2021-08-06 01:55:04 +0900

    추가: 현재 페이지 url 데이터 출력 기능 추가

--- actProjects/App/views.py
+++ actProjects/App/views.py
@@ -8,20 +8,29 @@ import os
 
 # Create your views here.
 def home(request) :
-    return JsonResponse({"request":'home.html'})
+    return JsonResponse({"request": 'home.html'})
 
 # 검색결과 페이지
 def search_play(request):
     return JsonResponse({"request": 'searchPage.html'})
 
 # 검색 결과 페이지, 더보기 클릭 (GET /api/search/:type)
-def search_detail(request, type):
+def search_detail(request, type, start):
     keyword = request.GET.get('query', "")
     plays = models.Play.objects.filter(title__icontains=keyword)  # play 모든 결과 불러옴
-    #nextLink =
 
     search_list = []  # 검색 결과들
     data_start = 0 # 앞으로 가져올 데이터의 시작점
+    #print(request.resolver_match.url_name)
+    #print(request.resolver_match.view_name)
+    print(request.get_full_path())
+
+    if start:
+        next = request.get_full_path().split("&start")[0]
+        + "&start"
+        + string(start+10)
+    else:
+        next = request.get_full_path() + "&start=11"
 
     # 검색 결과가 0개 일 때
     if len(plays) == 0:
@@ -45,8 +54,7 @@ def search_detail(request, type):
     # 더보기를 눌러서 더 많은 공연을 조회했을 때
     return JsonResponse({
         "links": {
-            # next 가 null이 아니라면
-            #"next": string ,
+            "next": next
         },
         "data": {
             "query": keyword,
