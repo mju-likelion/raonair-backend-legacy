933d37ff9873cb94226fa145c5bf86237c59ca91
Bhanjo <hanjo1515@naver.com>
2021-08-10 22:40:53 +0900

    수정: 코드리뷰 피드백 반영

--- actProjects/App/models.py
+++ actProjects/App/models.py
@@ -81,8 +81,6 @@ class Comment(models.Model):
     comment = models.CharField(max_length=200)
     play = models.ForeignKey('Play', models.DO_NOTHING, db_column='play')
     user = models.ForeignKey('User', models.DO_NOTHING, db_column='user')
-    updated_at = models.DateTimeField()
-    created_at = models.DateTimeField()
 
     class Meta:
         managed = False
@@ -138,8 +136,6 @@ class DjangoSession(models.Model):
 class Like(models.Model):
     play = models.ForeignKey('Play', models.DO_NOTHING, db_column='play')
     user = models.ForeignKey('User', models.DO_NOTHING, db_column='user')
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
 
     class Meta:
         managed = False
@@ -150,8 +146,6 @@ class Person(models.Model):
     name = models.CharField(max_length=255)
     photo = models.CharField(
         unique=True, max_length=255, blank=True, null=True)
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
 
     class Meta:
         managed = False
@@ -179,8 +173,6 @@ class Play(models.Model):
     # Field name made lowercase.
     culturegov_external_link = models.CharField(
         db_column='cultureGov_external_link', unique=True, max_length=255, blank=True, null=True)
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
 
     class Meta:
         managed = False
@@ -193,8 +185,6 @@ class Staff(models.Model):
     play = models.ForeignKey(Play, models.DO_NOTHING,
                              db_column='play', blank=True, null=True)
     role = models.CharField(max_length=8)
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
 
     class Meta:
         managed = False
@@ -205,8 +195,6 @@ class Star(models.Model):
     play = models.ForeignKey(Play, models.DO_NOTHING, db_column='play')
     user = models.ForeignKey('User', models.DO_NOTHING, db_column='user')
     star = models.DecimalField(max_digits=2, decimal_places=0)
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
 
     class Meta:
         managed = False
@@ -218,8 +206,6 @@ class Team(models.Model):
         Person, models.DO_NOTHING, db_column='person', blank=True, null=True)
     troupe = models.ForeignKey(
         'Troupe', models.DO_NOTHING, db_column='troupe', blank=True, null=True)
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
 
     class Meta:
         managed = False
@@ -234,8 +220,6 @@ class Theater(models.Model):
         max_digits=11, decimal_places=8, blank=True, null=True)
     latitude = models.DecimalField(
         max_digits=10, decimal_places=8, blank=True, null=True)
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
 
     class Meta:
         managed = False
@@ -246,8 +230,6 @@ class Troupe(models.Model):
     name = models.CharField(unique=True, max_length=255)
     type = models.CharField(max_length=7)
     logo = models.CharField(unique=True, max_length=255, blank=True, null=True)
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
 
     class Meta:
         managed = False
@@ -259,8 +241,6 @@ class TroupeLike(models.Model):
         Troupe, models.DO_NOTHING, db_column='troupe', blank=True, null=True)
     user = models.ForeignKey('User', models.DO_NOTHING,
                              db_column='user', blank=True, null=True)
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
 
     class Meta:
         managed = False
@@ -273,8 +253,6 @@ class User(models.Model):
     name = models.CharField(max_length=10)
     nickname = models.CharField(unique=True, max_length=10)
     email_confirmed = models.IntegerField()
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
 
     class Meta:
         managed = False
