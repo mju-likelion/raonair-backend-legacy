d7ba481f4b9209f0ae21ffe0a139be023f4eb428
Bhanjo <hanjo1515@naver.com>
2021-08-11 14:58:42 +0900

    수정: cors header setting 변경

--- actProjects/actProject/settings.py
+++ actProjects/actProject/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'App.apps.AppConfig',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -53,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'actProject.urls'
